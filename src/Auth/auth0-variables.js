5967f79bf1972f24d0f4dcb9496631baef00c5aa
prakash <prakash32596@gmail.com>
2018-10-01 17:10:23 +0530

    comited message

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  clientId: 'IkkYP0EwNSYzuuQX2KseYSJsqpk4yyAg',
+  domain: 'nfriendship.eu.auth0.com',
+  callbackUrl: 'http://localhost:8080/callback',
+  apiUrl: 'https://nfriendship.eu.auth0.com/api/v2/'
+}
